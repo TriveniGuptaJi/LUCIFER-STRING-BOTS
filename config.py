ef8473b062fdc62755afe8205e3e1633f6797e1f
RAJ RAUTTRAY <jasminbibiworkweb45@gmail.com>
2025-08-01 01:30:28 +0530

    XQUEEN

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_ID = int(getenv("OWNER_ID","7532420990"))
 
 MONGO_DB_URI = getenv("mongodb+srv://xO9AW3OvKg8MmPFy:xO9AW3OvKg8MmPFy@cluster0.uo4mhiz.mongodb.net/?retryWrites=true&w=majority")
-MUST_JOIN = getenv("MUST_JOIN","ARAME9")
+MUST_JOIN = getenv("MUST_JOIN","LuciferBotss")
